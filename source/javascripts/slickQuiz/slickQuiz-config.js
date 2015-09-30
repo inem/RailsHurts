38904efd8f9065c187e292408dd9e7aead192f75
Ivan Nemytchenko <nemytchenko@gmail.com>
2015-09-30 17:40:20 +0200

    quiz fixes

--- source/javascripts/slickQuiz/slickQuiz-config.js
+++ source/javascripts/slickQuiz/slickQuiz-config.js
@@ -4,14 +4,14 @@
 
 var quizJSON = {
     "info": {
-        "name":    "Test Your Knowledge!!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
-        "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
-        "level1":  "Jeopardy Ready",
-        "level2":  "Jeopardy Contender",
-        "level3":  "Jeopardy Amateur",
-        "level4":  "Jeopardy Newb",
-        "level5":  "Stay in school, kid..." // no comma here
+        "name":    "Ruby or Ruby on Rails?",
+        "main":    "Let's check whether you remember where Ruby ends and Rails starts",
+        "results": "<h5>Learn More</h5><p></p>",
+        "level1":  "Ruby expert",
+        "level2":  "Perfect",
+        "level3":  "Not bad",
+        "level4":  "Nice try",
+        "level5":  "Seriously?" // no comma here
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
